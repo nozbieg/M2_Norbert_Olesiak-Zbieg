56575d95670828f078c527994ce506a0ffef0c3b
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-01-09 17:37:22 +0100

    Initial commit

---
+++ src/main/java/com/nolesiak/bookstoreapp/domain/Book.java
@@ -0,0 +1,19 @@
+package com.nolesiak.bookstoreapp.domain;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    String status;
+}
