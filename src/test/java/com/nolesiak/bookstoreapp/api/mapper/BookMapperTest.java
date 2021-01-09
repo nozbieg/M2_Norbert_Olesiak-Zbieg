50f023ad30ccf50e040b338e7dca6da530c2c1c1
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-01-09 18:25:59 +0100

    First test

--- src/test/java/com/nolesiak/bookstoreapp/api/mapper/BookMapperTest.java
+++ src/test/java/com/nolesiak/bookstoreapp/api/mapper/BookMapperTest.java
@@ -1,5 +1,7 @@
 package com.nolesiak.bookstoreapp.api.mapper;
 
+import com.nolesiak.bookstoreapp.api.model.BookDTO;
+import com.nolesiak.bookstoreapp.domain.Book;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,10 +13,17 @@ class BookMapperTest {
     @Test
     void bookToBookDTO() {
         //given
-
+        Book book = new Book();
+        book.setId(2L);
+        book.setTitle("Simple book");
+        book.setStatus("available");
 
         //when
+        BookDTO bookDTO = bookMapper.bookToBookDTO(book);
 
         //then
+        assertEquals(Long.valueOf(2L), bookDTO.getId());
+        assertEquals("Simple book", bookDTO.getTitle());
+        assertEquals("available", bookDTO.getStatus());
     }
 }
