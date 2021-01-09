a9ec0a3520ff2a1a3987ab27f02e37db4053b202
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-01-09 18:19:32 +0100

    mapper class

---
+++ src/main/java/com/nolesiak/bookstoreapp/api/mapper/BookMapper.java
@@ -0,0 +1,15 @@
+package com.nolesiak.bookstoreapp.api.mapper;
+
+
+import com.nolesiak.bookstoreapp.api.model.BookDTO;
+import com.nolesiak.bookstoreapp.domain.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    BookDTO bookToBookDTO(Book book);
+}
---
+++ src/test/java/com/nolesiak/bookstoreapp/api/mapper/BookMapperTest.java
@@ -0,0 +1,20 @@
+package com.nolesiak.bookstoreapp.api.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookMapperTest {
+
+    BookMapper bookMapper = BookMapper.INSTANCE;
+
+    @Test
+    void bookToBookDTO() {
+        //given
+
+
+        //when
+
+        //then
+    }
+}
