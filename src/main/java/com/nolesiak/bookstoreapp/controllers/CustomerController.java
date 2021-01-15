2a9796268bf8f22780ce94fedc6ab1913f7b78a7
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-01-15 21:48:54 +0100

    controller modification

--- src/main/java/com/nolesiak/bookstoreapp/controllers/CustomerController.java
+++ src/main/java/com/nolesiak/bookstoreapp/controllers/CustomerController.java
@@ -3,13 +3,12 @@ package com.nolesiak.bookstoreapp.controllers;
 import com.nolesiak.bookstoreapp.api.mapper.BookMapper;
 import com.nolesiak.bookstoreapp.api.model.BookDTO;
 import com.nolesiak.bookstoreapp.api.model.CustomerDTO;
-import com.nolesiak.bookstoreapp.domain.Book;
 import com.nolesiak.bookstoreapp.services.CustomerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestController
@@ -44,8 +43,12 @@ public class CustomerController {
                                 .map(bookMapper::bookToBookDTO)
                                 .collect(Collectors.toList());
     }
-
-    //CustomerDTO createNewCustomer(CustomerDTO customerDTO);
-
-    //CustomerDTO updateCustomer(Long customerID, CustomerDTO customerDTO);
+    @PostMapping
+    public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO){
+        return new ResponseEntity<CustomerDTO>(customerService.createNewCustomer(customerDTO), HttpStatus.CREATED);
+    }
+    @PutMapping("updateCustomer/{customerID}")
+    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long customerID,@RequestBody CustomerDTO customerDTO) {
+        return new ResponseEntity<CustomerDTO>(customerService.updateCustomer(customerID, customerDTO), HttpStatus.OK);
+    }
 }
